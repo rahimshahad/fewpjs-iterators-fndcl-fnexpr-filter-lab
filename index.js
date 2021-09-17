b6b582461810db57b4b72deb02f35065cefa4729
rahimshahad <rahim.shahad2@gmail.com>
2021-09-17 13:33:46 -0400

    commit tree 6b5fd019939a442cc0613a77284ebb0e52988dff

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(driver => {
+        if (driver.toLowerCase() === name.toLowerCase()) {
+            return driver
+        }
+    })
+
+}
+
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(driver => {
+        return driver.toLowerCase().indexOf(name.toLowerCase()) === 0
+    })
+}
+
+function matchName(drivers, name) {
+
+    return drivers.filter(driver => {
+
+        if (driver.name.toLowerCase() === name.toLowerCase()) {
+            return driver
+        }
+
+
+    });
+
+}
